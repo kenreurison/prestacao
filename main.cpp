cbbe13332b322aab721f7e71b1c5793bacbb57ee
kenreurison <kenreurison@gmail.com>
2016-02-02 17:30:01 -0300

    Utilização de Script para criação do Banco de dados.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <QCoreApplication>
 #include "sqlitecolaborador.h"
+#include "qfile.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    SqliteColaborador colaborador = SqliteColaborador();
+    QFile file("SQLITEdatabase.db");
+    if(!file.exists()){
+        QSqlDatabase mydb = QSqlDatabase::addDatabase("QSQLITE");
+        mydb.setDatabaseName("SQLITEdatabase.db");
+        mydb.setConnectOptions("foreign_keys = ON");
+        mydb.open();
+        QSqlQuery query(mydb);
+        QString script = file2String("sqliteCreateDatabase.txt");
+        query.exec(script);
+
+    }
     return a.exec();
 }
 
+QString file2String(QString file);
