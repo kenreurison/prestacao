b63d24ab1374853c84a7d579e123740f87a38cfd
kenreurison <kenreurison@gmail.com>
2016-02-02 20:57:53 -0300

    Código incompleto para separar a QString do sql dividido pelos ";" utilizando para isso o Split.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,23 @@
 #include <QCoreApplication>
 #include "sqlitecolaborador.h"
 #include "qfile.h"
-#include "qtextcodec.h"
+#include "qtextstream.h"
+
 
 QString file2String(QString _file){
-    QFile file(_file);
-    file.open(QFile::ReadOnly|QFile::Text);
-    QString result = QTextCodec::codecForMib(106)->toUnicode(file.readAll());
-    result.
+    QFile inputFile(_file);
+    QString result = "";
+    if (inputFile.open(QIODevice::ReadOnly))
+    {
+       QTextStream in(&inputFile);
+       while (!in.atEnd())
+       {
+          QString line = in.readLine();
+          result.append(line);
+       }
+       inputFile.close();
+    }
+    return result;
 }
 
 
@@ -15,7 +25,7 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     QFile file("SQLITEdatabase.db");
-    file.remove();
+    //file.remove();
     QString script("sqliteCreateDatabase.txt");
     if(!file.exists()){
         file.close();
@@ -24,8 +34,28 @@ int main(int argc, char *argv[])
         mydb.setConnectOptions("foreign_keys = ON");
         mydb.open();
         QSqlQuery query(mydb);
-        QString scriptString = file2String(script);
-        query.exec(scriptString);
+        query.exec(file2String(script));
+        /*query.exec("CREATE TABLE db_colaborador ("
+                       "pk_colaborador	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
+                       "col_nome INTEGER"
+                   ");");
+        query.exec("CREATE TABLE db_detalhes ("
+                       "pk_detalhes INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
+                       "col_parcelamento INTEGER,"
+                       "col_valor_total FLOAT,"
+                       "col_data_vencimento INTEGER,"
+                       "col_data_cartao TEXT"
+                   ");");
+        query.exec("create table db_transferencia("
+                       "col_descricao TEXT,"
+                       "col_comentarios TEXT,"
+                       "pk_transferencia INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
+                       "fk_colaborador_out INTEGER,"
+                       "fk_colaborador_in INTEGER,"
+                       "fk_detalhes INTEGER,"
+                       "FOREIGN KEY (fk_detalhes) REFERENCES db_opcao(pk_detalhes),"
+                       "FOREIGN KEY(fk_colaborador_out, fk_colaborador_in) REFERENCES db_colaborador(pk_colaborador, pk_colaborador)"
+                   ");");*/
     }
     return a.exec();
 }
