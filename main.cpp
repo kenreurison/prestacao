a98a65f45cc4ac0d3e5f57baff6dcf756fe68c85
kenreurison <kenreurison@gmail.com>
2016-02-02 18:46:28 -0300

    Necessário remover as quebras de linhas do arquivo para criação do banco de dados. E é necessário também verificar qual a pasta visível do QT.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,31 @@
 #include <QCoreApplication>
 #include "sqlitecolaborador.h"
 #include "qfile.h"
+#include "qtextcodec.h"
+
+QString file2String(QString _file){
+    QFile file(_file);
+    file.open(QFile::ReadOnly|QFile::Text);
+    QString result = QTextCodec::codecForMib(106)->toUnicode(file.readAll());
+    result.
+}
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     QFile file("SQLITEdatabase.db");
+    file.remove();
+    QString script("sqliteCreateDatabase.txt");
     if(!file.exists()){
+        file.close();
         QSqlDatabase mydb = QSqlDatabase::addDatabase("QSQLITE");
         mydb.setDatabaseName("SQLITEdatabase.db");
         mydb.setConnectOptions("foreign_keys = ON");
         mydb.open();
         QSqlQuery query(mydb);
-        QString script = file2String("sqliteCreateDatabase.txt");
-        query.exec(script);
-
+        QString scriptString = file2String(script);
+        query.exec(scriptString);
     }
     return a.exec();
 }
-
-QString file2String(QString file);
