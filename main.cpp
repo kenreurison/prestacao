9c893f833b6c468fc91ae7e969228fb950bdb147
kenreurison <kenreurison@gmail.com>
2016-02-03 09:50:47 -0300

    Criação do banco automaticamente através de um script sql.

--- main.cpp
+++ main.cpp
@@ -2,23 +2,24 @@
 #include "sqlitecolaborador.h"
 #include "qfile.h"
 #include "qtextstream.h"
+#include "QStringList"
 
 
-QString file2StringList(QString _file){
+QStringList file2StringList(QString _file){
     QFile inputFile(_file);
-    QString result = "";
+    QString listresult = "";
     if (inputFile.open(QIODevice::ReadOnly))
     {
        QTextStream in(&inputFile);
        while (!in.atEnd())
        {
           QString line = in.readLine();
-          result.append(line);
+          listresult.append(line);
        }
        inputFile.close();
     }
-    QStringList listresult.split(";");
-    return result;
+    QStringList stringList = listresult.split(";");
+    return stringList;
 }
 
 
@@ -35,29 +36,10 @@ int main(int argc, char *argv[])
         mydb.setConnectOptions("foreign_keys = ON");
         mydb.open();
         QSqlQuery query(mydb);
-        QStringList file2StringList(script)
-        query.exec();
-        /*query.exec("CREATE TABLE db_colaborador ("
-                       "pk_colaborador	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
-                       "col_nome INTEGER"
-                   ");");
-        query.exec("CREATE TABLE db_detalhes ("
-                       "pk_detalhes INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
-                       "col_parcelamento INTEGER,"
-                       "col_valor_total FLOAT,"
-                       "col_data_vencimento INTEGER,"
-                       "col_data_cartao TEXT"
-                   ");");
-        query.exec("create table db_transferencia("
-                       "col_descricao TEXT,"
-                       "col_comentarios TEXT,"
-                       "pk_transferencia INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
-                       "fk_colaborador_out INTEGER,"
-                       "fk_colaborador_in INTEGER,"
-                       "fk_detalhes INTEGER,"
-                       "FOREIGN KEY (fk_detalhes) REFERENCES db_opcao(pk_detalhes),"
-                       "FOREIGN KEY(fk_colaborador_out, fk_colaborador_in) REFERENCES db_colaborador(pk_colaborador, pk_colaborador)"
-                   ");");*/
+        QStringList stringList = file2StringList(script);
+        for(int i=0; i<stringList.length(); i++){
+            query.exec(((QString) stringList.at(i)).append(";"));
+        }
     }
     return a.exec();
 }
