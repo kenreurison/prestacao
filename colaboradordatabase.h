9ec05db52daf4d7598840fb40fa32d216b62a78f
kenreurison <kenreurison@gmail.com>
2016-02-12 17:06:48 -0300

    Copia de arquivos para garantir inicio de programa.

--- colaboradordatabase.h
+++ colaboradordatabase.h
@@ -1,6 +1,7 @@
 #ifndef COLABORADORDATABASE_H
 #define COLABORADORDATABASE_H
 #include <QSqlDatabase>
+#include "qfile.h"
 
 class ColaboradorDatabase
 {
@@ -13,6 +14,13 @@ private:
         mydb.setConnectOptions("foreign_keys = ON");
         mydb.open();
     }
+    void runSqlDump(){
+        QFile *f = new QFile(":/banco/script/SQLITEdatabase.db");
+        if(!f->exists()){
+            QFile::copy(":/banco/script/SQLITEdatabase.db", "SQLITEdatabase.db");
+        }
+    }
+
 public:
     static QSqlDatabase getInstance(){
         if(!instance)
